384511d8fee269dacf8e789672c8bee74e220337
Dongxu Li <dongxuli2011@gmail.com>
2025-04-02 15:33:19 -0400

    msvc

--- scripts/build-windows.bat
+++ scripts/build-windows.bat
@@ -1,12 +1,15 @@
 call set-windows-env.bat
 
 rem qmake6.exe librecad.pro -r -spec win32-g++
-cd
+cd ${{gitub.workspace}}
+cd 
 mkdir build
 
 pushed build
 cmake.exe .. -G "Visual Studio 17 2022"
 cmake --build . --config Release
+rem msbuild /p:Configuration=${{ env.BUILD_CONFIGURATION }} /p:OutDir=${{ runner.temp }}\build\ "${{ env.SOLUTION_FILE_PATH }}\QtStockV3.sln"
+
 
 dir *.exe
 if NOT exist LibreCAD.exe (
