f579fbf094b19174cc0ad23125ebcdab8a569f67
Dongxu Li <dongxuli2011@gmail.com>
2025-04-03 03:56:03 -0400

    NSIS

--- scripts/build-windows.bat
+++ scripts/build-windows.bat
@@ -11,17 +11,15 @@ cmake.exe .. -G "Visual Studio 17 2022"
 cmake --build . --config Release
 rem msbuild /p:Configuration=${{ env.BUILD_CONFIGURATION }} /p:OutDir=${{ runner.temp }}\build\ "${{ env.SOLUTION_FILE_PATH }}\QtStockV3.sln"
 
-
 dir *.exe
-if NOT exist LibreCAD.exe (
-	echo "Building windows\LibreCAD.exe failed!"
+if NOT exist Release\LibreCAD.exe (
+	echo "Building Release\LibreCAD.exe failed!"
 	exit /b /1
 )
 
 set
-windeployqt6.exe --release LibreCAD--compiler-runtime
-cd ..
+windeployqt6.exe --release Release\LibreCAD --compiler-runtime
 cd
-
+cd scripts
 
 call build-win-setup.bat
