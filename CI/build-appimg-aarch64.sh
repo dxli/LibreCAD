d073a358a27dbf14f4441d302c2e9d7d95aa69e1
Dongxu Li <dongxuli2011@gmail.com>
2026-03-02 21:10:22 -0800

    cleanup

--- CI/build-appimg-aarch64.sh
+++ CI/build-appimg-aarch64.sh
@@ -46,7 +46,7 @@ cp unix/librecad appdir/usr/bin/
 cp unix/plugins/*/*.so appdir/usr/lib/librecad/
 cp -r unix/resources/qm appdir/usr/share/librecad/
 # rsync -av --include='*/' --include='*.json' --exclude='*' plugins/ appdir/usr/lib/librecad/
-find plugins/*/ -iname "*.json" -exec cp -f '{}' appdir/usr/lib/librecad/ ';'
+#find plugins/*/ -iname "*.json" -exec cp -f '{}' appdir/usr/lib/librecad/ ';'
 rsync -av --include='*/' --include='*.qm' --exclude='*' plugins/ appdir/usr/share/librecad/qm/
 
 cp desktop/librecad.desktop appdir/usr/share/applications/
