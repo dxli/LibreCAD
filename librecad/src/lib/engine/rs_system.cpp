1f5ec8fdd3d1d06199550a8efd0899302fbafab9
Dongxu Li <dongxuli2011@gmail.com>
2026-03-15 19:42:36 -0700

    translations/

--- librecad/src/lib/engine/rs_system.cpp
+++ librecad/src/lib/engine/rs_system.cpp
@@ -628,7 +628,7 @@ QStringList RS_System::getDirectoryList(const QString& _subDirectory) {
     dirList.append(executableDirectory + "/resources/" + subDirectory);
 
     if (QStringLiteral("qm") == subDirectory) {
-        dirList.append(QDir::cleanPath(executableDirectory + "/translations"));
+        dirList.append(QDir::cleanPath(executableDirectory + "/../translations"));
     }
 
 #if !(defined(Q_OS_WIN32) || defined(Q_OS_WIN64))
