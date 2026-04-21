192979046e332236518f67cb9609db4b445ec21a
Dongxu Li <dongxuli2011@gmail.com>
2026-04-22 02:19:53 +0800

    cmake fixes

--- scripts/build-win-setup.bat
+++ scripts/build-win-setup.bat
@@ -9,5 +9,17 @@ if _%LC_ARCH%==_ (
 )
 
 pushd postprocess-windows
-makensis.exe /X"SetCompressor /FINAL lzma" /V4 /D%LC_ARCH% %LC_NSIS_FILE%
+
+rem Build NSIS flags with architecture
+set NSIS_FLAGS=/X"SetCompressor /FINAL lzma" /V4 /D%LC_ARCH%
+
+rem Pass SCMREVISION and VIProductVersion if defined
+if NOT "_%SCMREVISION%"=="_" (
+    set NSIS_FLAGS=!NSIS_FLAGS! /DSCMREVISION="%SCMREVISION%"
+)
+if NOT "_%VIProductVersion%"=="_" (
+    set NSIS_FLAGS=!NSIS_FLAGS! /DVIProductVersion="%VIProductVersion%"
+)
+
+makensis.exe !NSIS_FLAGS! %LC_NSIS_FILE%
 popd
